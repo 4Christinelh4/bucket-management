85f30735ceee199f38207567c58dce7ffab82297
Yuwei Liang <z5433878@ad.unsw.edu.au>
2024-03-09 02:16:48 +1100

    add a test

--- bucketmanagement_test.go
+++ bucketmanagement_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
-	"github.com/aws/jsii-runtime-go"
+	// "github.com/aws/aws-cdk-go/awscdk/v2/awsiam"
+	// "github.com/aws/jsii-runtime-go"
 )
 
 // example tests. To run these tests, uncomment this file along with the
@@ -20,7 +22,29 @@ func TestBucketmanagementStack(t *testing.T) {
 	// THEN
 	template := assertions.Template_FromStack(stack, nil)
 
-	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
-		"VisibilityTimeout": 300,
-	})
+	roleType := "AWS::IAM::Role"
+	rawPropsSecurity := `                    "assumeRolePolicyDocument": {
+		"Statement": [
+		  {
+			"Action": "sts:AssumeRole",
+			"Effect": "Allow",
+			"Principal": {
+			  "AWS": "arn:aws:iam::654654234409:root"
+			}
+		  }
+		],
+		"Version": "2012-10-17"
+	  }`
+
+
+	  var documentAsString  map[string]interface{}
+
+	  json.Unmarshal([]byte(rawPropsSecurity), &documentAsString)
+
+	// test the security role 
+	template.HasResourceProperties( &roleType, documentAsString)
+
+	// template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
+	// 	"VisibilityTimeout": 300,
+	// })
 }
